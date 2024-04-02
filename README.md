5d751a465d44a20d4b1f085975c7214e531abe13
nieluoluo <281283522@qq.com>
2024-04-02 09:46:56 +0800

    save action

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# hello-world
-哈哈哈！ 这是我使用github的第一个存储库——hello world
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import Home, { useTestContext } from "./Home/Index";
+import { Allotment } from "allotment";
+import "allotment/dist/style.css";
+import test from "/test.png";
+import test1 from "/test.svg";
+
+function App() {
+  useEffect(() => {
+    let id = "src/Home1";
+    let string = new RegExp(`^${new RegExp(id)}`);
+    console.log(string);
+  }, []);
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vh",
+      }}
+    >
+      <img
+        style={{
+          width: "80px",
+        }}
+        src={test}
+      />
+      <img
+        style={{
+          width: "80px",
+        }}
+        src={test1}
+      />
+      <Allotment vertical={true}>
+        <Allotment.Pane minSize={200}>
+          <Allotment>
+            <Allotment.Pane minSize={200}>
+              <div>Pane 1</div>
+            </Allotment.Pane>
+            <Allotment.Pane minSize={200}>
+              <div>Pane 1</div>
+            </Allotment.Pane>
+          </Allotment>
+        </Allotment.Pane>
+        <Allotment.Pane minSize={200}>
+          <div>Pane 1</div>
+        </Allotment.Pane>
+      </Allotment>
+    </div>
+  );
+}
+
+export default App;
